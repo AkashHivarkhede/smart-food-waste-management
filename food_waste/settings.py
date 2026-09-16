73201da5605f23fa88c62fe2280d71b297f7a43e
Akash <sunny.hivarkhede@gmail.com>
2026-09-16 15:56:55 +0530

    Configure Django static files

--- food_waste/settings.py
+++ food_waste/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9@98jlf)4u2)1mgiuk#xv7305ju5xqihq33mnv_+!2cy_*5jds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["AkashHivarkhede.pythonanywhere.com"]
 
 
 # Application definition
@@ -128,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
