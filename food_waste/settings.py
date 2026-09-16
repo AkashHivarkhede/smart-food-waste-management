0f9ef6e90927b60388bdb997fff0ccb03b5b14e1
Akash <sunny.hivarkhede@gmail.com>
2026-09-16 15:20:49 +0530

    Configure SQLite database for deployment

--- food_waste/settings.py
+++ food_waste/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'food_waste.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'smart_food_waste',
+#         'USER': 'root',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smart_food_waste',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
